d768a034b9de542d18e407e21ceda7f128fd8cfb
wangzhongqiu <mautumn@outlook.com>
2018-11-08 16:50:51 +0800

    test

--- main.go
+++ main.go
@@ -8,20 +8,10 @@ import (
 
 	"github.com/Mautu/altermangetokafka/config"
 	"github.com/Mautu/altermangetokafka/sender"
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promauto"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"go.uber.org/zap"
 
 	model "github.com/Mautu/altermangetokafka/modle"
 	"github.com/gin-gonic/gin"
-)
-
-var (
-	getnotificationcount = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "getnotificationcount",
-		Help: "service get notification count",
-	})
 )
 
 func main() {
@@ -40,12 +30,9 @@ func main() {
 			zap.String("topic", config.Kafka.Topic),
 			zap.Duration("backoff", time.Second),
 		)
-		getnotificationcount.Inc()
 		sender.Sender(config.Kafka.Host, notification, config.Kafka.Topic)
 		c.JSON(http.StatusOK, gin.H{"message": " successful get altermange message"})
 	})
-	http.Handle("/metrics", promhttp.Handler())
-	http.ListenAndServe(":9690", nil)
 	router.Run(fmt.Sprintf("%s%s", ":", strconv.Itoa(config.Port)))
 
 }
